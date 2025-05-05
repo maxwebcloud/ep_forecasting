b374d93dcb2b0292b1fc1711d8a7c28a03961ea2
verenalaut <verena.lauter1@gmail.com>
2025-05-05 23:11:05 +0200

    cv_right copy contains all necessary workflow functions

--- naive_forecast.py
+++ naive_forecast.py
@@ -4,15 +4,15 @@ def naive_forecast(initial_split_preprocessed, scaler_y):
 
     _, y_test= initial_split_preprocessed[0]["test"]
     seasonal_lag = 7*24 #lag of one week 
-    y_test["predictions"] = y_test["actuals"].shift(seasonal_lag)
-    y_test=y_test.iloc[168:]
+    predictions = y_test[:-seasonal_lag]  # z. B. für seasonal_lag=1 → y_{t-1}
+    actuals = y_test[seasonal_lag:]
 
-    mse_scaled = mean_squared_error(y_test["actuals"], y_test["predictions"])
+    mse_scaled = mean_squared_error(actuals, predictions)
     rmse_scaled = np.sqrt(mse_scaled)
 
-    y_test['actuals'] = scaler_y.inverse_transform(y_test['actuals'].values.reshape(-1, 1))
-    y_test['predictions'] = scaler_y.inverse_transform(y_test['predictions'].values.reshape(-1, 1))
-    mse_orig = mean_squared_error(y_test["actuals"], y_test["predictions"])
+    actuals = scaler_y.inverse_transform(actuals)
+    predictions = scaler_y.inverse_transform(predictions)
+    mse_orig = mean_squared_error(actuals, predictions)
     rmse_orig = np.sqrt(mse_orig)
 
     return mse_scaled, rmse_scaled, mse_orig, rmse_orig
